0677330b680c89232c4eff236a196ed2d9098565
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-29 00:02:44 +0000

    Finished styling and coding different messages and if/then statements in JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+//
+// Created by: Maria G
+// Created on: April 28, 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function tempSent() {
+  //declaring original variables
+  let temperature = parseFloat(document.getElementById("temp-input").value);
+  let message = "";
+  //setting different if/then scenarios
+  if (temperature >= 15) {
+    message = "It is relatively hot out! You probably won't need a jacket, but you might want to wear sunscreen or a baseball cap.";
+  }
+  //else represents a temperature of less than 15 degrees
+  else {
+    message = "It is pretty chilly out! We suggest you wear some kind of jacket to stay warm.";
+  }
+  //display the correct message to the screen
+  document.getElementById("results").innerHTML = message;
+}
